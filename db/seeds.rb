5f8b9e032fc8089ad98ca8bfe8220f75ebff83ff
hydref <charles.dutheil@gmail.com>
2019-01-31 00:03:46 +0100

    THC DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+ cour = Cour.create!(content: Faker::Shakespeare.hamlet_quote)
+end
+
+
+15.times do
+ student = Student.create!(name: Faker::LordOfTheRings.character, cour_id: rand(1..5))
+end
